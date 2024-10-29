490fba4daa0c360da016a47a70d990badf8d7aba
Oscar Espinosa <oscardo2012@gmail.com>
2024-10-28 21:34:30 -0500

    Se agrega validación de token en rutas

--- src/routes/documentos.ts
+++ src/routes/documentos.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import * as middlewares from '../middlewares/token.middleware';
 import * as documentosIdentidadController from '../controllers/documentos-identidad.controller';
 
 const router = express.Router();
 
-router.get('/documentos/tipo', documentosIdentidadController.listarTiposDocumento);
-router.post('/documentos/digito-verificacion', documentosIdentidadController.calcularDigitoVerificacion);
+router.get('/documentos/tipo', middlewares.verificarToken, documentosIdentidadController.listarTiposDocumento);
+router.post('/documentos/digito-verificacion', middlewares.verificarToken, documentosIdentidadController.calcularDigitoVerificacion);
 
 
 export default router;
