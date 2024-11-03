a0a2b08f8d231aab04b905137a0a90ee5cdbcc2c
Oscar Espinosa <oscardo2012@gmail.com>
2024-11-02 21:27:07 -0500

    Ajustes de error al iniciar sesión

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -2,21 +2,26 @@ import { Request, Response } from "express";
 import * as tokenService from '../services/token.service';
 const usuarioModel = require('../models/usuario.model');
 
-
 export async function iniciarSesion(req: Request, res: Response) {
     const { usuario, clave } = req.body;
     let respuesta = await usuarioModel.logueaUsuario(usuario, clave);
-    if (respuesta[0].Mensaje) {
-        res.status(401).send({ "codigo": "-1", "mensaje": respuesta[0].Mensaje });
-    } else {
-        const idUsuarioString = { id_usuario: respuesta[0].Id_Usuario };
-        const token = tokenService.generarToken(idUsuarioString);
-        res.status(200)
-            .set('Authorization', `Bearer ${token}`)
-            .send({
-                "codigo": "1",
-                "mensaje": "usuario encontrado",
-                "data": respuesta
-            });
+    try{
+        if (respuesta.length == 0 || respuesta[0]?.Mensaje) {
+            const mensaje = respuesta[0]?.Mensaje || 'Usuario no encontrado';
+            res.status(401).send({ "codigo": "-1", "mensaje": mensaje });
+        } else {
+            const idUsuarioString = { id_usuario: respuesta[0].Id_Usuario };
+            const token = tokenService.generarToken(idUsuarioString);
+            res.status(200)
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    "codigo": "1",
+                    "mensaje": "usuario encontrado",
+                    "data": respuesta
+                });
+        }
+    } catch(ex){
+        console.log("Se presentó un error: " + ex);
+        throw(ex);
     }
 }
