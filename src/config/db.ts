466de04096879552143f3892a2cda301145b89c2
Diego Andres Mateus Triana <diego.mateus.triana.contractor@bbva.com>
2025-01-22 23:08:27 -0500

    update PRD DB Data

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,9 @@
 module.exports = {
-    server: "3.128.128.131",
-    port: 1433,
+    server: "190.26.219.126",
+    port: 21433,
     database: "PPM",
     user: "UsrPpm",
-    password: "UsrPpm135*",
+    password: "UsrPpmP4$$w0rd",
     options: {
         encrypt: false,
         trustServerCertificate: true
